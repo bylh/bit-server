07ef8647bd9428545ccb54953841e9351cdea291
li huan <li.huan@zhtu.net>
2019-07-16 23:10:26 +0800

    add environment

---
+++ bit/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  BaseClientUrl: 'https://bylh.top',
+  BaseServerUrl: 'https://bylh.top:5000',
+  Firebase: {
+      apiKey: 'AIzaSyCQ9UmGm7rfR4_mGv9wbvpYt2YpEIR1uIo',
+      authDomain: 'bylh-web.firebaseapp.com',
+      databaseURL: 'https://bylh-web.firebaseio.com',
+      projectId: 'bylh-web',
+      storageBucket: 'bylh-web.appspot.com',
+      messagingSenderId: '339273342568'
+  },
+Pwa: {
+      serverPublicKey: 'BJ3kbCc44PMG9THjY4Nc-JqYKsUkd64e-n4oFGErmuAuFfunVUK1hqrqLOHEO_L1KJQhAZgZSn4F8lUZCYhPRfk'
+  }
+};
---
+++ bit/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  BaseClientUrl: 'https://localhost:4200',
+  BaseServerUrl: 'http://localhost:5001',
+  // BaseClientUrl: 'https://bylh.top',
+  // BaseServerUrl: 'https://bylh.top:5000',
+  Firebase: {
+      apiKey: 'AIzaSyCQ9UmGm7rfR4_mGv9wbvpYt2YpEIR1uIo',
+      authDomain: 'bylh-web.firebaseapp.com',
+      databaseURL: 'https://bylh-web.firebaseio.com',
+      projectId: 'bylh-web',
+      storageBucket: 'bylh-web.appspot.com',
+      messagingSenderId: '339273342568'
+  },
+Pwa: {
+      serverPublicKey: 'BJ3kbCc44PMG9THjY4Nc-JqYKsUkd64e-n4oFGErmuAuFfunVUK1hqrqLOHEO_L1KJQhAZgZSn4F8lUZCYhPRfk'
+  }
+};
